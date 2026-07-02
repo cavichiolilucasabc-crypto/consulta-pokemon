0ce9118ffd420384c634961ff0a990ec65369066
cavichiolilucasabc-crypto <cavichiolilucasabc@gmail.com>
2026-07-02 13:34:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function buscarCarta() {
+  const campo = document.getElementById("campoCarta");
+  const termo = campo.value.trim();
+
+  if (termo === "") {
+    alert("Digite o nome ou código da carta.");
+    return;
+  }
+
+  const termoFormatado = encodeURIComponent(termo);
+
+  const urlLiga = `https://www.ligapokemon.com.br/?card=${termoFormatado}&view=cards/search`;
+
+  window.open(urlLiga, "_blank");
+}
